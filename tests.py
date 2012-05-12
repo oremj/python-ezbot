b16e80220fbf90fde8f0b014990b5f14e52b36fd
Jeremy Orem <jeremy.orem@gmail.com>
2012-05-11 17:43:22 -0700

    Better test coverage.

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ def test_register_hooks():
     c = conn()
 
     r = '^test'
-    f = lambda x: x
+    f = lambda: c.send('testmsg', True)
 
     c.register_init_hook(f)
     c.register_chanmsg_hook(r, f)
@@ -25,6 +25,9 @@ def test_register_hooks():
     ok_((r, f) in c._chanmsg_hooks)
     ok_((r, f) in c._privmsg_hooks)
 
+    
+    c._run_init_hooks()
+    c._sock.sendall.assert_called_with('testmsg\r\n')
 
 def test_process():
     c = conn()
@@ -39,8 +42,12 @@ def test_process():
     c._sock.sendall.assert_called_with('PONG id\r\n')
 
     assert c._registered == False
+    c._sock.sendall.reset_mock()
+    c.send('testmsg')
+    assert c._sock.sendall.call_count == 0
     c._process(':testhost.test.com 001')
     assert c._registered == True
+    c._sock.sendall.assert_called_with('testmsg\r\n')
 
     c._process(':testuser!testhsot.test.com '
                'PRIVMSG #testchannel :test message')
@@ -54,3 +61,18 @@ def test_join_channel():
     c._registered = True
     c.join_channel('#testchannel')
     c._sock.sendall.assert_called_with('JOIN #testchannel\r\n')
+
+
+def test_readline():
+    c = conn()
+    c._sock.recv.return_value = "test1\ntest2\n"
+    assert c._readline() == "test1"
+    assert c._readline() == "test2"
+
+
+def test_auth():
+    c = conn()
+    c._registered = True
+    c._auth('testuser')
+    c._sock.sendall.assert_any_call('NICK testuser\r\n')
+    c._sock.sendall.assert_called_with('USER testuser testhost foo :testuser\r\n')
